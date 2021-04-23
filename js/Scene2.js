d46cb16714665958be81806c89c2e3fd0fb3bea7
Robert <quizzez1912@gmail.com>
2021-04-23 13:01:28 +0200

    physic

--- js/Scene2.js
+++ js/Scene2.js
@@ -5,38 +5,131 @@ class Scene2 extends Phaser.Scene{
     }
 
     create() {
-        this.background = this.add.image(0,0,"background");
+        this.background = this.add.tileSprite(0,0,config.width, config.height ,"background");
         this.background.setOrigin(0,0);    
 
-        this.ship1 = this.add.image(100,100,"ship");
-        this.ship2 = this.add.image(50,100, "ship2");
-        this.ship3 = this.add.image(150,100, "ship3");
-
-        
+        this.ship1 = this.add.sprite(config.width / 2 - 50 , config.height / 2 , "ship");
+        this.ship2 = this.add.sprite(config.width / 2 , config.height / 2 , "ship2");
+        this.ship3 = this.add.sprite(config.width / 2 + 50 , config.height / 2 , "ship3");
         
         this.add.text(20,20, "Playing game", {font: "25px Arial",fill:"yellow"});
-    }
 
-    moveShip(ship,speed) {
-        ship.y += speed;
-      
-        console.log(ship.y);
+        // Animation ship
+        this.anims.create({
+            key: "ship1_anim",                                  // Animation Name ship_anim
+            frames: this.anims.generateFrameNumbers("ship"),    // Using the frames from the "ship" spritesheet
+            frameRate:20,                                       // 20 frames per Second
+            repeat: -1,                                          //  -1 = infinite repeat
+                                                 
+        });
 
-        if(ship.y > config.height){
-            console.log("out of map");
-            this.resetShipPos(ship);
+        // Animation ship2
+        this.anims.create({
+            key: "ship2_anim",                                  // Animation Name ship2_anim
+            frames: this.anims.generateFrameNumbers("ship2"),    // Using the frames from the "ship2" spritesheet
+            frameRate:20,                                       // 20 frames per Second
+            repeat: -1                                              //  -1 = infinite repeat
+        });
+
+        // Animation ship3
+        this.anims.create({
+        key: "ship3_anim",                                        // Animation Name ship3_anim
+        frames: this.anims.generateFrameNumbers("ship3"),         // Using the frames from the "ship3" spritesheet
+        frameRate:20,                                                   // 20 frames per Second
+        repeat: -1                                              //  -1 = infinite repeat
+        });
+
+        // Animation explosion
+        this.anims.create({
+            key: "explode",                                                 // Animation Name explode
+            frames: this.anims.generateFrameNumbers("explosion"),        // Using the frames from the "explosion" spritesheet
+            frameRate:20,                                                // 20 frames per Second
+            repeat: 0,
+            hideOnComplete: true                                            
+        });
+
+        this.anims.create({
+            key:"red",                                  // Animation Name red
+            frames:this.anims.generateFrameNumbers("power-up",{
+                start: 0,
+                end:1
+            }),
+            frameRate: 20,
+            repeat : -1
+
+        });
+
+        this.anims.create({
+            key:"gray",                                  // Animation Name red
+            frames:this.anims.generateFrameNumbers("power-up",{
+                start: 2,
+                end:3
+            }),
+            frameRate: 20,
+            repeat : -1
+
+        });
+
+        this.powerUps = this.physics.add.group();
+
+        var maxObjects = 4;
+        for (var i = 0; i <= maxObjects;i++){
+           var powerUp = this.physics.add.sprite(16, 16, "power-up");
+            this.powerUps.add(this.powerUp);
+            powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
         }
 
+
+
+
+        this.ship1.play("ship1_anim");
+        this.ship2.play("ship2_anim");
+        this.ship3.play("ship3_anim");
+
+
+
+
     }
 
-    resetShipPos(ship) {
-        ship.y = 0;
-        var randomX = Phaser.Math.Between(0,config.width);
-        ship.x = randomX;
-    }
+        moveShip(ship,speed) {
+            ship.y += speed;
+        
+        
+
+            if(ship.y > config.height){
+                
+                this.resetShipPos(ship);
+            }
+
+        }
+
+        resetShipPos(ship) {
+            ship.y = 0;
+            var randomX = Phaser.Math.Between(0,config.width);
+            ship.x = randomX;
+
+        }
+       
+        destroyShip(pointer, gameObject) {
+            gameObject.setTexture("explosion"); // set the Texture of the ship to explosion
+            gameObject.play("explode");         // explode animation getting triggered
+        }
+        
+        
+    
     update() {
         this.moveShip(this.ship1,1);
         this.moveShip(this.ship2,2);
         this.moveShip(this.ship3,3);
+
+        this.ship1.setInteractive();
+        this.ship2.setInteractive();
+        this.ship3.setInteractive();
+
+        this.input.on("gameobjectdown", this.destroyShip,this)
+
+        
+
+        this.background.tilePositionY -= 0.5;
     }
 }
