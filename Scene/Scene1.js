af0dbb19fe60bbf0a4a50f0c303ee363b1e1413f
sexyjana123 <janaphung.1434@gmail.com>
2021-05-01 21:44:19 +0200

    hat iwie nicht gesaved

--- Scene/Scene1.js
+++ Scene/Scene1.js
@@ -52,6 +52,11 @@ class Scene1 extends Phaser.Scene {
         // Wasabi
         this.load.image("wasabi","assets/spritesheets/game/wasabi.png");
 
+        //Soyfish
+        this.load.image("soyfish","assets/spritesheets/game/soyfish.png");
+
+        //Chopstick
+        this.load.image("chopstick","assets/spritesheets/game/chopstick.png");
 
         //* UI Elemente
         // Lebensanzeige
